5f0070043d55d0b4f3e0134b96b3f0af32c19ba5
Everton <evertonulidigital@gmail.com>
2024-06-01 16:09:26 -0300

    Projeto Final com novos testes de Login

--- webdriver-manager/src/test/java/EstudoTest.java
+++ webdriver-manager/src/test/java/EstudoTest.java
@@ -3,7 +3,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import java.time.Duration;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -47,9 +50,9 @@ public class EstudoTest {
     public void testCadastroSimplesComDadosValidos() {
         assertThat(driver.findElement(By.linkText("Cadastre-se")).getText(), is("Cadastre-se"));
         driver.findElement(By.linkText("Cadastre-se")).click();
-        driver.findElement(By.id("nome")).sendKeys("Ulisses25");
-        driver.findElement(By.id("email")).sendKeys("evertont25@uol.com.br");
-        driver.findElement(By.id("password")).sendKeys("12346");
+        driver.findElement(By.id("nome")).sendKeys("ulisses52");
+        driver.findElement(By.id("email")).sendKeys("ulisses52@uol.com.br");
+        driver.findElement(By.id("password")).sendKeys("123");
         driver.findElement(By.id("administrador")).click();
         assertEquals("Cadastrar", driver.findElement(By.cssSelector(".btn-primary")).getText(), "Texto esperado 'Cadastrar', mas foi exibido outro texto.");
         driver.findElement(By.cssSelector(".btn-primary")).click();
@@ -62,11 +65,30 @@ public class EstudoTest {
     public void testCadastroComDadosJaUtilizados() {
         assertThat(driver.findElement(By.linkText("Cadastre-se")).getText(), is("Cadastre-se"));
         driver.findElement(By.linkText("Cadastre-se")).click();
-        driver.findElement(By.id("nome")).sendKeys("Ulisses5");
-        driver.findElement(By.id("email")).sendKeys("evertont5@uol.com.br");
-        driver.findElement(By.id("password")).sendKeys("12346");
+        driver.findElement(By.id("nome")).sendKeys("ulisses52");
+        driver.findElement(By.id("email")).sendKeys("ulisses52@uol.com.br");
+        driver.findElement(By.id("password")).sendKeys("123");
         driver.findElement(By.id("administrador")).click();
         driver.findElement(By.cssSelector(".btn-primary")).click();
         assertEquals("Este email já está sendo usado", driver.findElement(By.cssSelector("div.alert.alert-secondary.alert-dismissible > span")).getText(), "Texto esperado 'Este email já está sendo usado', mas foi exibido outro texto");
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Teste realizado com Login ja cadastrado")
+    public void testLoginCaminhoFeliz() {
+        driver.findElement(By.id("email")).sendKeys("ulisses52@uol.com.br");
+        driver.findElement(By.id("password")).sendKeys("123");
+        driver.findElement(By.cssSelector("[data-testid='entrar']")).click();
+        assertEquals("Este é seu sistema para administrar seu ecommerce.", driver.findElement(By.cssSelector("p.lead")).getText(), "Texto esperado 'Este é seu sistema para administrar seu ecommerce.', mas foi exibido outro texto");
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Teste realizado com Login de usuario não cadastrado")
+    public void testLoginDadosInvalidos() {
+        driver.findElement(By.id("email")).sendKeys("ulisses52@uol.com.br");
+        driver.findElement(By.id("password")).sendKeys("123");
+        driver.findElement(By.cssSelector("[data-testid='entrar']")).click();
     }
 }
